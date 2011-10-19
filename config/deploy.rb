4716aa5b705eefe2776c23c715711c524a6d1c41
Vinicius Eduardo <viniciuseduardo.rj@gmail.com>
2011-10-19 08:22:09 -0200

    Commit Initial

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,78 @@
-set :application, "Ecommerce"
-set :repository,  "set your repository location here"
+require "bundler/capistrano"
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :application, "ecomerce"
+set :keep_releases, "3"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+# git options
+set :scm, "git"
+set :repository, "git@github.com:viniciuseduardo/Galgarpia.git"
+set :branch, "master"
+set :deploy_via, :remote_cache
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+# deploy credentials
+set :user, "root"
+set :deploy_to, "/var/www/#{application}"
+set :use_sudo, true
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+# server definitions
+set :servers, "178.79.171.141"
+
+role :app, servers
+role :web, servers
+role :db,  servers, :primary => true
+
+default_run_options[:pty] = true
+
+# working with Passenger
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  [:start, :stop].each do |t|
+    desc "don't need #{t} task with mod_rails"
+    task t, :roles => :app do
+      run "touch #{current_path}/tmp/restart.txt"
+    end
+  end
+end
+
+desc "Link in the production database.yml"
+task :after_update_code do
+  run "ln -nfs #{deploy_to}/#{shared_dir}/config/database.yml #{release_path}/config/database.yml"
+end
+
+# Only runs this task after deploy:setup
+# Used to create a new config/database.yml which differs from the repository
+namespace :init do
+  desc "Create production-only database.yml"
+  task :production_database_yml do
+    database_configuration =<<-EOF
+production:
+  adapter: mysql2
+  encoding: utf8
+  reconnect: false
+  database: galgarpia_production
+  pool: 5
+  host: 127.0.0.1
+  username: root
+  password:
+    EOF
+
+    run "mkdir -p #{shared_path}/config"
+    put database_configuration, "#{shared_path}/config/database.yml"
+  end
+  desc "Importa dados Teste"
+  task :import_data do
+    run "cd  #{current_path}; bundle exec rake RAILS_ENV=production  db:seed"
+  end
+  desc "Limpar arquivo log"
+  task :log_clear do
+    run "cd  #{current_path}; bundle exec rake log:clear;"
+  end
+end
+
+after "deploy:setup", "init:production_database_yml"
+after "deploy:cold", "init:import_data"
+after "deploy", "init:log_clear"
--- config/initializers/hoptoad.rb
+++
@@ -1,3 +0,0 @@
-HoptoadNotifier.configure do |config|
-  config.api_key = '1bb6d597bc210f8457b9e624bfe95b96'
-end
