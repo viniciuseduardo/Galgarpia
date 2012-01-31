49b71f33e208f7f3ed09745918535289b4c988c2
Vinicius Eduardo <viniciuseduardo.rj@gmail.com>
2012-01-31 05:10:59 -0200

    Finalizando

--- app/admin/sites.rb
+++ app/admin/sites.rb
@@ -16,7 +16,7 @@ ActiveAdmin.register Site do
         t.column("Price")       {|product| number_to_currency product.price }
         t.column("Total Sold")  {|product| Order.find_with_product(product).where("payment_status IS NOT NULL ").count.to_s }
         t.column("Total Sales Value"){|product| number_to_currency Order.find_with_product(product).where("payment_status IS NOT NULL ").sum(:price) }
-        t.column(){|product| link_to "Remove", admin_product_path(product), :method => :delete, :confirm => "Quer remover esse produto?"}
+        t.column(){|product| link_to "Remove", admin_produto_path(product), :method => :delete, :confirm => "Quer remover esse produto?"}
       end
     end
 
